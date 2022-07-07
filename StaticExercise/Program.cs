f11f61e64d9b3a06dc73b528dd86f807319ce8d8
Dustin Key <Keydustin90@gmail.com>
2022-07-07 07:39:23 -0500

    done

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var celcius = TempConverter.FahrenheitToCelsius(68);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+
+
+            Console.WriteLine($"Celcius {celcius}");
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+        }
+    }
+}
